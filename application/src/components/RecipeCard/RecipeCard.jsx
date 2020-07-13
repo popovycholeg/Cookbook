8cd83d26516682a974fe70bccea0a9e70f086d07
Oleg Popovych <magmoba@gmail.com>
2020-07-13 22:10:57 +0300

    Expanded card

--- application/src/components/RecipeCard/RecipeCard.jsx
+++ application/src/components/RecipeCard/RecipeCard.jsx
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
+import Avatar from "@material-ui/core/Avatar";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
+import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
+import CardHeader from "@material-ui/core/CardHeader";
 import Typography from "@material-ui/core/Typography";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import clsx from "clsx";
+import Collapse from "@material-ui/core/Collapse";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 import withHocs from "./RecipeCardHoc";
 import RecipeDialog from "../RecipeDialog/RecipeDialog";
 
 function RecipeCard(props) {
   const [openDialog, setOpenDialog] = useState(false);
+  const [expanded, setExpanded] = useState(false);
   const { classes, data, onOpen } = props;
-  const { id, name, description } = data;
+  const { id, name, description, dateCreated } = data;
+  const date = new Date(dateCreated * 1000).toLocaleString();
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
     <React.Fragment>
@@ -24,10 +41,14 @@ function RecipeCard(props) {
       />
       <Grid item xs={12} sm={6} md={4}>
         <Card className={classes.card}>
+          <CardHeader
+            avatar={<Avatar aria-label='recipe'>OP</Avatar>}
+            title={name}
+            subheader={date}
+          />
           <CardMedia
             className={classes.cardMedia}
             image='https://source.unsplash.com/random'
-            title='Image title'
           />
           <CardContent className={classes.cardContent}>
             <Typography gutterBottom variant='h5' component='h2'>
@@ -35,24 +56,69 @@ function RecipeCard(props) {
             </Typography>
             <Typography>{description}</Typography>
           </CardContent>
-          <CardActions>
-            <Button size='small' color='primary'>
-              View
-            </Button>
+          <CardActions disableSpacing>
+            {/* <IconButton aria-label='add to favorites'>
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label='share'>
+              <ShareIcon />
+            </IconButton> */}
             <Button size='small' color='primary' onClick={() => onOpen(data)}>
               Edit
             </Button>
-            <Button
-              size='small'
-              color='secondary'
-              onClick={() => setOpenDialog(true)}
-            >
+            <Button size='small'color='secondary' onClick={() => setOpenDialog(true)} >
               DELETE
             </Button>
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label='show more'
+            >
+              <ExpandMoreIcon />
+            </IconButton>
           </CardActions>
+          <Collapse in={expanded} timeout='auto' unmountOnExit>
+            <CardContent>
+              <Typography paragraph>Method:</Typography>
+              <Typography paragraph>
+                Heat 1/2 cup of the broth in a pot until simmering, add saffron
+                and set aside for 10 minutes.
+              </Typography>
+              <Typography paragraph>
+                Heat oil in a (14- to 16-inch) paella pan or a large, deep
+                skillet over medium-high heat. Add chicken, shrimp and chorizo,
+                and cook, stirring occasionally until lightly browned, 6 to 8
+                minutes. Transfer shrimp to a large plate and set aside, leaving
+                chicken and chorizo in the pan. Add pimentón, bay leaves,
+                garlic, tomatoes, onion, salt and pepper, and cook, stirring
+                often until thickened and fragrant, about 10 minutes. Add
+                saffron broth and remaining 4 1/2 cups chicken broth; bring to a
+                boil.
+              </Typography>
+              <Typography paragraph>
+                Add rice and stir very gently to distribute. Top with artichokes
+                and peppers, and cook without stirring, until most of the liquid
+                is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
+                reserved shrimp and mussels, tucking them down into the rice,
+                and cook again without stirring, until mussels have opened and
+                rice is just tender, 5 to 7 minutes more. (Discard any mussels
+                that don’t open.)
+              </Typography>
+              <Typography>
+                Set aside off of the heat to let rest for 10 minutes, and then
+                serve.
+              </Typography>
+            </CardContent>
+          </Collapse>
+          {/* <CardActions>
+            
+          </CardActions> */}
         </Card>
       </Grid>
-      </React.Fragment>
+    </React.Fragment>
   );
 }
 
--- application/src/components/RecipeCard/styles.js
+++ application/src/components/RecipeCard/styles.js
@@ -9,5 +9,22 @@ export const styles = theme => ({
   },
   cardContent: {
     flexGrow: 1
+  },
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
   }
 });
